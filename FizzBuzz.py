340188126530a99ba8518d3859ae1cd50582e152
CassieOlivas <clolivas@ucdavis.edu>
2022-11-01 12:49:50 -0700

    Script that iterates through 100 numbers and displays the number or a select output if divisible by 3, 5, or both.

---
+++ FizzBuzz.py
@@ -0,0 +1,16 @@
+#Prints the numbers 1-100, except for:
+    # If the number is divisible by 3, print "Fizz" instead.
+    # If the number is divisible by 5, print "Buzz" instead.
+    # If the number is divisible by 3 and 5, print "FizzBuzz" instead.
+
+a_list = list(range(1,100))
+
+for x in a_list:
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
