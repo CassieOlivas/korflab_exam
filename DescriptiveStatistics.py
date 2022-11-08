007dba026170922e3995a274f00e0b9a1dc02121
Cassandra Olivas <betta.fische@gmail.com>
2022-11-07 21:42:40 -0800

    Calculates the mean, median, and standard deviation of user-inputted numbers. It ignores illegal values.

--- DescriptiveStatistics.py
+++ DescriptiveStatistics.py
@@ -1,8 +1,9 @@
-# Program calculates the median, mean, and standard deviation of values that are given on the command line.
+# Program calculates the mean, median, and standard deviation of values that are given on the command line.
 #   Illegal values are skipped, but generate a warning that is sent to stderr.
 
-import numpy
+import numpy as np
 import sys
+import pandas as pd
 
 print("Enter your numbers on the line below and I will calculate the median, mean, and standard deviation.")
 userInput = (input("Please separate the numbers with a space: "))
@@ -11,13 +12,11 @@ val = userInput.split()
 
 newVal = []
 
-badChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-           'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0']
-
 for x in val:
     tempVal = x
     if tempVal.isdigit() == False:
-        sys.stderr.write("Incorrect value type. Next time please use only integers. \n")
+        sys.stderr.write("Warning, " + tempVal + " is an incorrect value type. Next time please use only integers. \n")
+        # Considers floats as an incorrect value type.
     else:
         newVal.append(int(x))
 
@@ -26,14 +25,51 @@ for x in val:
 # for x in newVal:
 #     print(type(x))
 
-# Calculate the mean.
+# # Calculate the mean.
 sum, count = 0, 0
 
 for x in newVal:
     sum = sum + x
     count += 1
 
-print("The mean is: " + str(sum/count))
+print("Mean: " + str(sum/count))
 
 # Calculate the median.
+midVal = len(newVal)/2
 
+arrayVal = np.array(newVal)
+
+if float(midVal).is_integer() == True:
+    #Length of list is even.
+    intVal = int(midVal)
+    firstHalf = newVal[:intVal]
+    secondHalf = newVal[intVal:]
+    tempVal = (firstHalf[intVal - 1] + secondHalf[0]) / 2
+    print("Median: " + str(tempVal))
+else:
+    #Length of list is odd.
+    tempVal = arrayVal[int(midVal)]
+    print("Median: " + str(tempVal))
+
+# Calculate the standard deviation.
+# # Calculate the mean.
+meanVal = (sum/count)
+
+arraySD = []
+
+# # Find the square of the distance from the mean.
+for x in newVal:
+    tempVal = x - meanVal
+    tempVal2 = tempVal**2
+    arraySD.append(tempVal2)
+
+# # Sum of the values.
+sumSD = 0
+
+for x in arraySD:
+    sumSD = sumSD + x
+
+# # Divide by the number of values and take the square root.
+standDev = sumSD / len(newVal)
+standDevSQRT = standDev**(1/2)
+print("Standard Deviation: " + str(standDevSQRT))
