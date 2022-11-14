f59484f4019ae1c9b506ebb3b5e432ef6c172519
Cassandra Olivas <betta.fische@gmail.com>
2022-11-13 17:31:57 -0800

    Calculates the local hydrophilicity/hydrophobicity of a sequence.

--- Hydropathy.py
+++ Hydropathy.py
@@ -2,34 +2,74 @@
 # The sequence and window size are command line parameters.
 # The output is tabular with columns for position and average hydropathy at the position.
 
-# userInput_Seq = (input("Please insert your sequence: "))
-# userInput_Window = (input("Please give the window length: "))
+import numpy as np
+
+userInput_Seq = (input("Please insert your amino acid sequence: "))
+userInput_Window = (input("Please give the window length: "))
+
+dict_Values = {'I': 4.5,
+               'V': 4.2,
+               'L': 3.8,
+               'F': 2.8,
+               'C': 2.5,
+               'M': 1.9,
+               'A': 1.8,
+               'G': -0.4,
+               'T': -0.7,
+               'S': -0.8,
+               'W': -0.9,
+               'Y': -1.3,
+               'P': -1.6,
+               'H': -3.2,
+               'E': -3.5,
+               'Q': -3.5,
+               'D': -3.5,
+               'N': -3.5,
+               'K': -3.9,
+               'R': -4.5
+}
 
 
-userInput_seq = "ACAAGATGCCAT"
+#Declare variables.
+seq_Array = list(userInput_seq)
+tempArray = []
+finalDict = {}
+sumVal = 0
+count = len(seq_Array)
 
-# userInput_seq = "ACAAGATGCCATTGTCCCCCGGCCTCCTGCTGCTGCTGCTCTCCGGGGCCACGGCCACCGCTGCCCTGCC" \
-#                 "CCTGGAGGGTGGCCCCACCGGCCGAGACAGCGAGCATATGCAGGAAGCGGCAGGAATAAGGAAAAGCAGC" \
-#                 "CTCCTGACTTTCCTCGCTTGGTGGTTTGAGTGGACCTCCCAGGCCAGTGCCGGGCCCCTCATAGGAGAGG" \
-#                 "AAGCTCGGGAGGTGGCCAGGCGGCAGGAAGGCGCACCCCCCCAGCAATCCGCGCGCCGGGACAGAATGCC" \
-#                 "CTGCAGGAACTTCTTCTGGAAGACCTTCTCCTCCTGCAAATAAAACCTCACCCATGAATGCTCACGCAAG" \
-#                 "TTTAATTACAGACCTGAA"
+# # Hydrophilicity/hydrophobicity for the entire sequence ignoring non-key parameters.
+# for x in seq_Array:
+#     if x in dict_Values:
+#         print(x, dict_Values[x])
 
-userInput_window = 3
+# Average hydrophilicity/hydrophobicity based on the sequence window.
+if len(seq_Array) % userInput_Window == 0: #Check if the entire sequence will be easily divided by the window, or if it'll have a remainder.
+    for x in seq_Array:
+        tempArray.append(x)
+        sumVal = sumVal + dict_Values[x]
+        if len(tempArray) == userInput_Window:
+            strKey = ''.join(tempArray)
+            tempDict = {strKey: sumVal}
+            finalDict.update(tempDict)
+            # Empty values for next iteration.
+            sumVal = 0
+            tempArray = []
+else:
+    for x in seq_Array:
+        tempArray.append(x)
+        sumVal = sumVal + dict_Values[x]
+        count -= 1
+        if len(tempArray) == userInput_Window:
+            strKey = ''.join(tempArray)
+            tempDict = {strKey: sumVal}
+            finalDict.update(tempDict)
+            # Empty values for next iteration.
+            sumVal = 0
+            tempArray = []
+        elif count < (len(seq_Array) % userInput_Window):
+            strKey = ''.join(tempArray)
+            tempDict = {strKey: sumVal}
+            finalDict.update(tempDict)
 
-seq_arrayTemp = []
-seq_dict = {}
-count = 0
-
-for x in userInput_seq:
-    seq_arrayTemp.append(x)
-    if len(seq_arrayTemp) == userInput_window:
-        count += 1
-        seq_dict[count].append(x)
-
-
-print(len(userInput_seq))
-print(seq_arrayTemp)
-print(len(seq_arrayTemp))
-print(seq_dict)
-
+for x in finalDict:
+    print(x, round(finalDict[x], 5))
